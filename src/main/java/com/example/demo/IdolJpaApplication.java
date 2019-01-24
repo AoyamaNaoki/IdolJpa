d425dd2bae63d1480854d70b716eccbe81179727
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2019-01-24 18:36:10 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/IdolJpaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdolJpaApplication.class, args);
+	}
+
+}
+
